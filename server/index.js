ab553121aee49c4060759271dd4057226c24096f
Icarz <mr.rghay@email.com>
2024-10-15 16:38:52 +0100

    verify

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { adminRouter } from "./Routes/AdminRoute.js";
 import { EmployeeRouter } from "./Routes/EmployeeRoute.js";
+import jwt from "jsonwebtoken";
 
 // creating and instance from express server
 const app = express();
@@ -18,6 +19,16 @@ app.use("/auth", adminRouter);
 app.use("/employee", EmployeeRouter);
 app.use(express.static("public"));
 
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    jwt.verify(token, "jwt_secret_key");
+  } else {
+    res.json({ Status: false, Error: "Note authenticated!!" });
+  }
+};
+
+app.use("/verify", verifyUser, (req, res) => {});
 // checking the server configuration
 app.listen(3000, () => {
   console.log("server is running");
